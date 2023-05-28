cf4aa4de37d154489e6a3815333a945b7edef0af
willsantos96 <willsantos96@gmail.com>
2023-05-28 15:12:02 -0300

    create url edit

--- visitas/urls.py
+++ visitas/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.visitas, name='home'),
     path('add_visitas/', views.add_visitas, name='add_visitas'),
     path('listar_visitas/', views.listar_visitas, name='listar_visitas'),
-    path('editar_visitas/', views.editar_visitas, name='editar_visitas'),
+    path('editar_visitas/<int:visita_id>/', views.editar_visitas, name='editar_visitas'),
     path('excluir_visitas/', views.excluir_visitas, name='excluir_visitas'),
 ]
--- visitas/views.py
+++ visitas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from visitas import models
 from .models import Visitante
 
@@ -12,8 +12,23 @@ def listar_visitas (request):
     visitantes = Visitante.objects.all()
     return render(request, 'visitas/listar_visitas.html', {'visitantes': visitantes})
 
-def editar_visitas (request):
-    return render(request, 'visitas/editar_visitas.html')
+def editar_visitas (request, visita_id):
+    # Recupera o visitante com o ID fornecido ou retorna um erro 404 se não encontrado
+    visitante = get_object_or_404(Visitante, id=visita_id)
+    if request.method == 'POST':
+        # Se o formulário for enviado via POST, atualize os dados do visitante com os novos valores
+        visitante.nome = request.POST['nome']
+        visitante.doc = request.POST['doc']
+        visitante.telefone = request.POST['telefone']
+        # Atualize outros campos conforme necessário
+
+        # Salve as alterações no banco de dados
+        visitante.save()
+
+        # Redirecione para a página de listagem de visitantes ou para uma página de sucesso
+        return redirect('visitas:listar_visitas')
+
+    return render(request, 'visitas/editar_visitas.html', {'visitantes': visitante})
 
 def excluir_visitas (request):
     return render(request, 'visitas/excluir_visitas.html')
