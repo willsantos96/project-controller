822c172d95b339891335bbf175531444160129ee
willsantos96 <willsantos96@gmail.com>
2023-05-31 07:22:22 -0300

    redirect dps edit

--- visitas/views.py
+++ visitas/views.py
@@ -3,25 +3,30 @@ from visitas import models
 from .models import Visitante
 from .forms import VisitanteForm
 
-def visitas (request):
+
+def visitas(request):
     return render(request, 'visitas/home.html')
 
-def add_visitas (request):
+
+def add_visitas(request):
     if request.method == 'POST':
         form = VisitanteForm(request.POST)
         if form.is_valid():
             form.save()  # Salva o objeto Visitante no banco de dados
-            return redirect('visitas:listar_visitas')  # Redireciona para a página de listagem de visitas
+            # Redireciona para a página de listagem de visitas
+            return redirect('visitas:listar_visitas')
     else:
         form = VisitanteForm()
-    
+
     return render(request, 'visitas/add_visitas.html', {'form': form})
 
-def listar_visitas (request):
+
+def listar_visitas(request):
     visitantes = Visitante.objects.all()
     return render(request, 'visitas/listar_visitas.html', {'visitantes': visitantes})
 
-def editar_visitas (request, visita_id):
+
+def editar_visitas(request, visita_id):
     visitantes = Visitante.objects.all()
     # Recupera o visitante com o ID fornecido ou retorna um erro 404 se não encontrado
     visitante = get_object_or_404(Visitante, id=visita_id)
@@ -34,12 +39,12 @@ def editar_visitas (request, visita_id):
         # Salve as alterações no banco de dados
         visitante.save()
 
-        # Redirecione para a página de listagem de visitantes ou para uma página de sucesso
-        return redirect('visitas:editar_visitas',{'form': form})
+        return redirect('visitas:listar_visitas')
 
     return render(request, 'visitas/editar_visitas.html', {'visitantes': visitante})
 
-def excluir_visitas (request, visita_id):
+
+def excluir_visitas(request, visita_id):
     visitante = get_object_or_404(Visitante, pk=visita_id)
 
     if request.method == 'POST':
