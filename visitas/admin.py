0b2c5329c33ec760c5241ac78d36348fcf6a02fa
willsantos96 <willsantos96@gmail.com>
2023-05-31 06:47:44 -0300

    config area adm django

--- visitas/admin.py
+++ visitas/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Visitante
 
 
-admin.site.register(Visitante)
+class VisitanteAdmin (admin.ModelAdmin):
+    list_display = ('id', 'nome', 'telefone')
+
+
+admin.site.register(Visitante, VisitanteAdmin)
